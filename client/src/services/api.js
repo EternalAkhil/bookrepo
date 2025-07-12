7108c46480b755a672382b5b24cb661499a1d21e
EternalAkhil <akr142003@gmail.com>
2025-07-12 10:42:00 +0530

    Connected with backend deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const apiUrl = import.meta.env.VITE_API_URL;
+const API = axios.create({ baseURL: apiUrl });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("user"));
